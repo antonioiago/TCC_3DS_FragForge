6c047fcb42832d777363c780d72fd577bf2cd3ff
RyanRG <BlankHoleRRG@hotmail.com>
2026-03-04 15:16:08 -0300

    começo da finalização dos formularios

--- Site_E-Sports/cadastro.php
+++ Site_E-Sports/cadastro.php
@@ -1,55 +1,13 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="style.css">
-    <title>FragForce</title>
-</head>
-<body>
-<style>
-    header, main{
-        background-color: #135D04;
-    }
-    main{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-    }
-    main > *{
-        align-self: center;
-    }
-    form{
-        height: 320px;
-        width: 400px;
-        border-radius: 25px;
-        background-color: #888888;
-        justify-content: end;
-        display: flex;
-        flex-direction: column;
-        padding: 20px;
-        font-weight: bold;
-    }
-    label{
-        font-size: 12px;
-    }
-    p, label{
-        color: #eeeeee;
-    }
-    a{
-        color: #59aefd;
-        text-decoration-line: none;
-    }
-</style>
+<?php
+include __DIR__.'/includes/head.php';
+?>
+<link rel="stylesheet" href="styles/form.css">
 
 <main>
     <form method="POST" action="cadastrar.php">
         <?php
             session_start();
-            if (isset($_SESSION["MnsErro"])) {
-                echo '<p style="font-size: 15px;text-align: center; margin-bottom: 5px; color: rgb(245, 24, 30);font-weight:normal;">'.$_SESSION["MnsErro"].'</p>';
+            if (false) {
             }
         ?>
         <label>Email:</label><input type="text" name="email" placeholder="nome@local.com" required>
@@ -57,7 +15,7 @@
         <label>Repita a senha:</label><input type="password" name="chkpassword" required>
         <label>Data de nascimento:</label><input type="date" name= "birthdate" required>
         <label>Nome de Usuário</label><input type="text" name="nameuser" required>
-        <input style="margin-top:10px" type="submit" value="Cadastrar">
+        <input class="submit" type="submit" value="Cadastrar">
     </form>
     <p style="font-size: 15px">Já possui uma conta? <a href="login.php">Entre aqui!</a></p>
 </main>
--- Site_E-Sports/login.php
+++ Site_E-Sports/login.php
@@ -1,60 +1,24 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="style.css">
-    <title>FragForce</title>
-</head>
-<body>
-<style>
-    header, main{
-        background-color: #135D04;
-    }
-    main{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-    }
-    main > *{
-        align-self: center;
-    }
-    form{
-        height: 200px;
-        width: 400px;
-        border-radius: 25px;
-        background-color: #888888;
-        justify-content: end;
-        display: flex;
-        flex-direction: column;
-        padding: 20px;
-        font-weight: bold;
-    }
-    label{
-        font-size: 12px;
-    }
-    p, label{
-        color: #eeeeee;
-    }
-    a{
-        color: #59aefd;
-        text-decoration-line: none;
-    }
-</style>
+<?php
+include __DIR__.'/includes/head.php';
+?>
+<link rel="stylesheet" href="styles/form.css">
 
 <main>
-    <form method="POST">
-        <?php
-            if (true) {
-                echo '<p style="font-size: 15px;text-align: center; margin-bottom: 5px; color: rgb(245, 24, 30);font-weight:normal;">Login invalido, tente novamente.</p>';
-            }
-        ?>
-        <label>Email/Nickname:</label><input type="text">
-        <label>Senha:</label><input type="text">
-        <input style="margin-top:10px" type="submit" value="Entrar">
-    </form>
-    <p style="font-size: 15px">Não possui conta? <a href="cadastro.php">Cadastre-se!</a></p>
+    <div>
+        <form method="POST">
+            <?php
+                if (false) {
+                    echo '<p style="font-size: 15px;text-align: center; margin-bottom: 5px; color: rgb(245, 24, 30);font-weight:normal;">Login invalido, tente novamente.</p>';
+                }
+            ?>
+            <label>Email/Nickname:</label><input type="text">
+            <label>Senha:</label><input type="text">
+            <input class="submit" type="submit" value="Entrar">
+        </form>
+        <div style="display: flex; justify-content: space-between">
+            <a href="cadastro.php">Não possui conta?</a>
+            <a href="cadastro.php">Esqueceu a senha?</a>
+        </div>
+    </div>
 </main>
 </body>
