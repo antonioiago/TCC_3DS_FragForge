69cb67002a3afca08eef1a3947424a5682751c6a
fernando <fernando@email>
2026-03-11 14:51:19 -0300

    mudanças no formulario de cadastro e criação do formulario de login

--- Site_E-Sports/login.php
+++ Site_E-Sports/login.php
@@ -13,7 +13,7 @@ include __DIR__.'/includes/head.php';
         <label>Nome de Usuário ou Email</label><input type="text" name="nickname_jogador || email_jogador" required>
         <label>Senha:</label><input type="password" name="senha_jogador" required>
         <input class="submit" type="submit" value="Logar">
-    <p style="font-size: 15px">Ainda não possuí uma conta?<a href="login.php">Clique aqui!</a></p>
+    <p style="font-size: 15px">Ainda não possuí uma conta?<a href="login.php">Clique aqui :)!</a></p>
 </main>
 
 
