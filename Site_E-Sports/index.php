ea372ffdcb87c6a5de5ef557743245d50d444615
antonioiago <antonio.portela@etec.sp.gov.br>
2026-03-16 17:42:03 -0300

    ata

--- Site_E-Sports/index.php
+++ Site_E-Sports/index.php
@@ -2,9 +2,57 @@
 INCLUDE __DIR__.'/includes/head.php';
 include __DIR__.'/includes/header.php';
 ?>
-    <main>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint repellendus voluptatem animi culpa est, incidunt porro vitae, at illo ad labore provident magnam expedita fugit. Aperiam cum dignissimos eius quas.</p>
-    </main>
+
+<main>
+    <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>FragForge</title>
+                <link rel="stylesheet" href="style/style.css">
+                <!---- esse link é so pra usar uma fonte que tem um icone bonito pro botão de login no header---->
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+                
+            </head>
+
+            <body>
+                <div class="boasv">
+                    <title>
+                        FragForge
+                    </title>
+                        <h2>
+                            <p>
+                                Bem vindo(a) ao FragForge! Sua última destinação para noticias de Overwatch!
+                            </p>
+                        </h2>
+                </div>
+
+
+                <div class="card">
+                    <img src="includes/imagens/overwatch2.jpg" alt="overwatch2">
+                        <title>
+                            Overwatch 2 
+                        </title>
+                            <p>
+                                <h2>
+                                Ex-diretor de Overwatch revela que Blizzard o ameaçou ao exigir lucros astronômicos do jogo
+                                </h2>
+                            </p>
+                        </a>
+                </div>
+
+
+      
+
+            </body>
+            </html>
+
+
+</main>
+    
+
+
 
 <?php
 include __DIR__.'/includes/footer.php';
