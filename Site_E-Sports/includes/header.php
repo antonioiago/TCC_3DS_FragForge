779ab7614f787ac45fbd8ed2c6111aa38d2c0639
RyanRG <BlankHoleRRG@hotmail.com>
2026-03-20 17:21:44 -0300

    Botão para sair no index bleeblelbelbelbelblebleblebleb

--- Site_E-Sports/includes/header.php
+++ Site_E-Sports/includes/header.php
@@ -2,12 +2,17 @@
     <h2>
         FragForge
     </h2>
-    
+    <style>
+        p{
+            display: inline;
+        }
+    </style>
+
     <?php
     session_start();
 
     if (isset($_SESSION['jogador']['id'])) {
-        echo "<p>Bem vindo, ".$_SESSION['jogador']['id']."</p>";
+        echo '<div><p>Bem vindo, '.$_SESSION['jogador']['id'].'</p><a class="btn-login" href="sair.php">Sair</a></div>';
     } else {
         echo '<p>Você não está logado! <a class="btn-login" href="form-login.php">Entre agora!</a>';
     }
